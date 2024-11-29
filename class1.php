6b5706f07742fcea950a58d28d645fc913c0fa50
ghazala810 <kasbi2233@gmail.com>
2024-11-28 22:19:13 -0800

    Add files via upload

---
+++ class1.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+// echo("PHP Programming");
+// echo ("<h1>PHP Programming</h1>");
+
+// $show = "PHP Programming";
+// echo("I am Leaning " . $show);
+
+// echo("<script>alert('php programming');</script>");
+
+ 
+// $num = 10;
+// $num_02 = 100;
+// echo($num > $num_02);
+
+?>                                                                               
